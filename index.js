9c1ba9946dd2b7b0c3e82834407b6d84689fe298
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-28 15:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ function BoardMember(name, homeState, training) {
   this.veto = function(){
     return "No, I must disagree"
   }
+
+  this.approve = function(){
+    return "You can do that!" 
+  }
+
+  this.doCharity = function(){
+    return "I like to help people." 
+
+  }
 }
